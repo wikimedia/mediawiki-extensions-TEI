4125585847d4a9f7a322da8547f408fe33e5292b
Tpt <thomaspt@hotmail.fr>
2019-03-29 13:53:53 +0100

    Adds basic JS CI configuration
    Change-Id: I69b454c8730494d3208deaae8d2e3d83951d769c

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+/* eslint-env node, es6 */
+module.exports = function ( grunt ) {
+	var conf = grunt.file.readJSON( 'extension.json' );
+
+	grunt.loadNpmTasks( 'grunt-banana-checker' );
+	grunt.loadNpmTasks( 'grunt-eslint' );
+	grunt.loadNpmTasks( 'grunt-jsonlint' );
+	grunt.loadNpmTasks( 'grunt-stylelint' );
+
+	grunt.initConfig( {
+		eslint: {
+			options: {
+				reportUnusedDisableDirectives: true,
+				cache: true
+			},
+			all: [
+				'**/*.js',
+				'!node_modules/**',
+				'!modules/jquery/*.js',
+				'!vendor/**'
+			]
+		},
+		stylelint: {
+			all: [
+				'modules/**/*.css'
+			]
+		},
+		banana: conf.MessagesDirs,
+		jsonlint: {
+			all: [
+				'**/*.json',
+				'!node_modules/**',
+				'!vendor/**'
+			]
+		}
+	} );
+
+	grunt.registerTask( 'test', [ 'eslint', 'stylelint', 'jsonlint', 'banana' ] );
+	grunt.registerTask( 'default', 'test' );
+};
